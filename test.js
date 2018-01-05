12fde0fd2e99e5b892da6ec3217d03700eed77e0
Joseph Burger <jburger@laborsync.com>
2018-01-04 21:27:49 -0500

    Removal of now for created and updated

--- test.js
+++ test.js
@@ -11,22 +11,23 @@ options.adapter = [
     adapter,
     {
         typeMap: typeMap,
-        databaseUrl: 'progres://laborsync:development@localhost:5432/data1'
+        url: 'progres://laborsync:development@localhost:5432/data1'
     }
 ];
 
+
 const store = fortune(typeDefinitions, options);
 
-let findIds;
-let findOptions;
+// let findIds;
+// let findOptions;
 
-// findIds = '1nQjQikoqsCHx6Mv';
+// // findIds = '1nQjQikoqsCHx6Mv';
 
-findOptions = {
-    match: {
-        email: 'glogan@crscompany.net'
-    },
-};
+// findOptions = {
+//     match: {
+//         email: 'glogan@crscompany.net'
+//     },
+// };
 
 // store.find('user', findIds, findOptions)
 //     .then((results) => {
@@ -34,30 +35,30 @@ findOptions = {
 //     })
 //     .catch(console.error);
 
-// store.create('token', [{
-//         account: 'EzYuN3UI21aOrPzU',
-//         user: 'Zgo85nn2TJrZdJa9',
-//         ip: '192.168.1.23',
-//         status: 'active'
-//     }]).then((results) => {
-//         console.log(results.payload);
-//     })
-//     .catch(console.error);
+store.create('token', [{
+        account: 'EzYuN3UI21aOrPzU',
+        user: 'Zgo85nn2TJrZdJa9',
+        ip: '192.168.1.23',
+        status: 'active'
+    }]).then((results) => {
+        console.log(results.payload);
+    })
+    .catch(console.error);
 
 // store.update('token', [{
-//         id: 'WXRvRF4cnQhaPRok',
+//         id: 'Qfr6gX0oVvbS2ePV',
 //         replace: {
 //             status: 'test',
-//             // updated: '2019-01-04 20:30:34.064165'
+//             updated: now,
 //         }
 //     }]).then((results) => {
 //         console.log(results.payload);
 //     })
 //     .catch(console.error);
 
-store.delete('token', [
-        'WXRvRF4cnQhaPRok'
-    ]).then((results) => {
-        console.log(results.payload);
-    })
-    .catch(console.error);
+// store.delete('token', [
+//         'WXRvRF4cnQhaPRok'
+//     ]).then((results) => {
+//         console.log(results.payload);
+//     })
+//     .catch(console.error);
